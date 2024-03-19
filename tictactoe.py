bb274725196a9a7b8fe15909b1a6021d25b2388b
Artur <arturgustavopj@gmail.com>
2024-03-19 19:40:23 -0300

    edit1

--- tictactoe.py
+++ tictactoe.py
@@ -7,31 +7,27 @@ class JogoDaVelha:
         ['1', '4', '7'], ['2', '5', '8'], ['3', '6', '9'],  # Verticais
         ['1', '5', '9'], ['3', '5', '7']  # Diagonais
     ]
-    JOGADOR_1 = 'X'
-    JOGADOR_2 = 'O'
     VAZIO = ' '
 
     def __init__(self):
         print("Bem vindo ao jogo da velha!")
-        jogador_inicial = random.choice([self.JOGADOR_1, self.JOGADOR_2])  # Para Escolher aleatoriamente quem começa
-        if jogador_inicial == self.JOGADOR_1:
-            self.simbolo_jogador_1 = 'X'
-            self.simbolo_jogador_2 = 'O'
-            self.turno = self.JOGADOR_1
-        else:
-            self.simbolo_jogador_1 = 'O'
-            self.simbolo_jogador_2 = 'X'
-            self.turno = self.JOGADOR_2
+        self.nome_jogador_1 = input("Nome do Jogador 1: ")
+        self.simbolo_jogador_1 = 'X'
+        self.nome_jogador_2 = input("Nome do Jogador 2: ")
+        self.simbolo_jogador_2 = 'O'
+
+        jogador_inicial = random.choice([self.nome_jogador_1, self.nome_jogador_2])  # Para Escolher aleatoriamente quem começa
+        self.turno = jogador_inicial
+        print(f"{self.turno} ({'X' if self.turno == self.nome_jogador_1 else 'O'}) começa.")
+
         self.tabuleiro = {str(i): self.VAZIO for i in range(1, 10)}
 
     def exibir_tabuleiro(self):
-
         print(f" {self.tabuleiro['1']} │ {self.tabuleiro['2']} │ {self.tabuleiro['3']} ")
         print("───┼───┼───")
         print(f" {self.tabuleiro['4']} │ {self.tabuleiro['5']} │ {self.tabuleiro['6']} ")
         print("───┼───┼───")
         print(f" {self.tabuleiro['7']} │ {self.tabuleiro['8']} │ {self.tabuleiro['9']} ")
-
 
     def verificar_ganhador(self):
         for combinacao in self.VITORIA_COMBINACOES:
@@ -55,17 +51,17 @@ class JogoDaVelha:
             jogada = input("Jogada: ")
 
             if self.tabuleiro.get(jogada) == self.VAZIO:
-                self.tabuleiro[jogada] = self.simbolo_jogador_1 if self.turno == self.JOGADOR_1 else self.simbolo_jogador_2
+                self.tabuleiro[jogada] = self.simbolo_jogador_1 if self.turno == self.nome_jogador_1 else self.simbolo_jogador_2
                 ganhador = self.verificar_ganhador()
 
                 if ganhador:
                     if ganhador == "empate":
                         print("EMPATE!!!")
                     else:
-                        print(f"{ganhador} é o vencedor!!!")
+                        print(f"{self.turno} ({self.simbolo_jogador_1 if self.turno == self.nome_jogador_1 else self.simbolo_jogador_2}) é o vencedor!!!")
                     break
                 else:
-                    self.turno = self.JOGADOR_1 if self.turno == self.JOGADOR_2 else self.JOGADOR_2
+                    self.turno = self.nome_jogador_1 if self.turno == self.nome_jogador_2 else self.nome_jogador_2
             else:
                 print(f"Jogada inválida, {jogada} já foi realizada ou não é uma posição válida.")
 
